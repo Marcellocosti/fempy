5c4c0e23f634448fa45acd15a6916c348bebd8a3
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-03-18 13:15:34 +0100

    add alice3 software options

--- fempy/sim/__init__.py
+++ fempy/sim/__init__.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import os
 # If you want to import alice3 sw add `export FEMPY_ALICE3=true` to your .bashrc
+=======
+# If you want to import alice3 sw add `export FEMPY_ALICE3=true` to your .bashrc
+import os
+>>>>>>> bef2228 (add alice3 software options)
 if os.getenv("FEMPY_ALICE3", 'false').lower() in ('true', '1'):
     from . import alice3
--- fempy/sim/alice3/__init__.py
+++ fempy/sim/alice3/__init__.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import os
 # If you want to import alice3 sw add `export FEMPY_ALICE3=true` to your .bashrc
 if os.getenv("FEMPY_ALICE3", 'false').lower() in ('true', '1'):
@@ -6,3 +7,8 @@ if os.getenv("FEMPY_ALICE3", 'false').lower() in ('true', '1'):
 =======
 #from . import alice3
 >>>>>>> 4b5d47b (comment alice3 config)
+=======
+# If you want to import alice3 sw add `export FEMPY_ALICE3=true` to your .bashrc
+if os.getenv("FEMPY_ALICE3", 'false').lower() in ('true', '1'):
+    from . import alice3
+>>>>>>> bef2228 (add alice3 software options)

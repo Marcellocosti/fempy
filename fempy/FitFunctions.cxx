fafa6b6b0b36aa1aab04a0489cdd004500522772
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-04-29 17:36:52 +0200

    Added weighted functions

--- fempy/FitFunctions.cxx
+++ fempy/FitFunctions.cxx
@@ -26,7 +26,10 @@ double PowerLaw(double *x, double *par);
 double FlatPol3(double *x, double *par);
 double FlatPol3PowLaw(double *x, double *par);
 double Pol3PowLaw(double *x, double *par);
+double WeightedPol3andPol3(double *x, double *par);
+double WeightedPol3andPol3Powlaw(double *x, double *par);
 double WeightedPol3andPol2(double *x, double *par);
+double WeightedPol3Pol3powlawAndPol2(double *x, double *par);
 double SillKStar(double *x, double *par);
 
 std::map<TString, std::tuple<double (*)(double *x, double *par), int>> functions = 
@@ -47,7 +50,10 @@ std::map<TString, std::tuple<double (*)(double *x, double *par), int>> functions
     {"flatpol3", {FlatPol3, 5}},
     {"flatpol3powlaw", {FlatPol3PowLaw, 6}},
     {"pol3powlaw", {Pol3PowLaw, 5}},
+    {"weighted_pol3_and_pol3", {WeightedPol3andPol3, 9}},
+    {"weighted_pol3_and_pol3powlaw", {WeightedPol3andPol3Powlaw, 10}},
     {"weightedpol3andpol2", {WeightedPol3andPol2, 12}},
+    {"weighted_pol3_pol3powlaw_and_pol2", {WeightedPol3Pol3powlawAndPol2, 13}},
     {"sillkstar", {SillKStar, 3}}};
 
 
@@ -75,8 +81,20 @@ double Pol3PowLaw(double *x, double *par) {
     return Pol3(x, par) * pow(x[0], par[4]); 
 }
 
+double WeightedPol3andPol3(double *x, double *par) {
+    return par[0] * Pol3(x, &par[1]) + (1 - par[0]) * Pol3(x, &par[5]); 
+}
+
+double WeightedPol3andPol3Powlaw(double *x, double *par) {
+    return par[0] * Pol3(x, &par[1]) + (1 - par[0]) * Pol3PowLaw(x, &par[5]); 
+}
+
 double WeightedPol3andPol2(double *x, double *par) {
     return par[0] * Pol3(x, &par[1]) + (1 - par[0]) * Pol3(x, &par[5]) + Pol2(x, &par[9]); 
+}
+
+double WeightedPol3Pol3powlawAndPol2(double *x, double *par) {
+    return par[0] * Pol3(x, &par[1]) + (1 - par[0]) * Pol3PowLaw(x, &par[5]) + Pol2(x, &par[10]); 
 }
 
 double PowerLaw(double *x, double *par) { 
